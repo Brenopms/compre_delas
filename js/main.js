9dbccef87e8e40afb3c72c68c73eae845ac776cd
Breno <brenosilva1@GMAIL.COM>
2018-05-29 19:11:10 -0300

    add footer and font-awesome

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// var input = document.querySelector('.email');
+// input.addEventListener('click', function(e) {
+//     if (this.value === ''){
+//         this.value = '';
+//         this.placeholder = '';
+//     }
+// });
+
+// input.addEventListener(blur, function(e){
+//     if (this.placeholder == ''){
+//         this.placeholder = 'Inscreva-se para receber atualizações!';
+//     };
+// });
